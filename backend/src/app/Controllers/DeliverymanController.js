d61ad29d9774ac4e20f46094cf9edfae1ddf22e2
RonaldoAntonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2020-01-29 23:53:58 -0300

    testando deliveryman list

--- backend/src/app/Controllers/DeliverymanController.js
+++ backend/src/app/Controllers/DeliverymanController.js
@@ -1,6 +1,27 @@
+import { Op } from 'sequelize';
 import CreateDeliveryman from '../Services/CreateDeliverymanService';
+import Deliveryman from '../Models/Deliveryman';
 
 export default {
+  async index(req, res) {
+    const { page = 1, quantity = 20, q: query = '' } = req.query;
+
+    const { rows: deliverys, count } = await Deliveryman.findAndCountAll({
+      limit: quantity,
+      offset: (page - 1) * quantity,
+      attributes: ['id', 'name', 'email', 'age'],
+      where: {
+        [Op.or]: [
+          { name: { [Op.substring]: `%${query}%` } },
+          { email: { [Op.substring]: `%${query}%` } },
+        ],
+      },
+      order: ['updated_at'],
+    });
+
+    const { deliverys: result } = { deliverys, count };
+    return res.set({ total_pages: Math.ceil(count / quantity) }).json(result);
+  },
   async store({ body }, res) {
     return res.json(await CreateDeliveryman.run(body));
   },

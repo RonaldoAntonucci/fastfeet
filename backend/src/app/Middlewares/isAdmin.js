42687af1e3a929337625d576869ef04071423204
RonaldoAntonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2020-01-29 21:50:30 -0300

    fix (backend): fix when not authenticated user

--- backend/src/app/Middlewares/isAdmin.js
+++ backend/src/app/Middlewares/isAdmin.js
@@ -1,5 +1,5 @@
-export default ({ auth: { isAdmin } }, res, next) => {
-  if (!isAdmin) {
+export default ({ auth }, res, next) => {
+  if (!auth || !auth.isAdmin) {
     return res.status(401).json({ error: 'Not permited, only for admins.' });
   }
   return next();

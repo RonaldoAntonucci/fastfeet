853999280d2bb07982e625b2dbe00dae9af14ad9
RonaldoAntonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2020-02-28 15:02:04 -0300

    feat(mobile): initial config

---
+++ mobile/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import { StatusBar } from 'react-native';
+import './config/reactotronConfig';
+
+import { store, persistor } from './store';
+import App from './App';
+
+import colors from '~/styles/colors';
+
+export default function Index() {
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <StatusBar barStyle="light-content" backgroundColor={colors.primary} />
+        <App />
+      </PersistGate>
+    </Provider>
+  );
+}

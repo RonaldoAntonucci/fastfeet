7fd889f66ffc2cbe326807c3be7b40832413ebef
RonaldoAntonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2020-02-29 03:44:27 -0300

    feat(mobile): deliveries profile

--- mobile/src/pages/Deliveries/index.js
+++ mobile/src/pages/Deliveries/index.js
@@ -1,12 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { View, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import {
+  Container,
+  ProfileContainer,
+  ProfileContent,
+  Avatar,
+  Welcome,
+  UserName,
+  SignOutButton,
+} from './styles';
+
+import { signOut } from '~/store/modules/auth/actions';
+import colors from '~/styles/colors';
 
 export default function Deliveries() {
+  const dispatch = useDispatch();
+  const user = useSelector(state => state.user.profile);
+
+  const [avatarImageUrl, setAvatarImageUrl] = useState();
+
+  useEffect(() => {
+    if (user.avatar_url) {
+      setAvatarImageUrl(user.avatar_url);
+    } else {
+      setAvatarImageUrl(
+        'https://api.adorable.io/avatars/50/abott@adorable.png'
+      );
+    }
+  }, [user]);
+
+  const handleLogOut = useCallback(() => {
+    dispatch(signOut());
+  }, [dispatch]);
+
   return (
-    <View>
-      <Text>Deliveries</Text>
-    </View>
+    <Container>
+      <ProfileContainer>
+        <ProfileContent>
+          <Avatar source={{ uri: avatarImageUrl }} />
+          <View>
+            <Welcome>Bem vindo de volta,</Welcome>
+            <UserName>{user.name}</UserName>
+          </View>
+        </ProfileContent>
+        <SignOutButton onPress={handleLogOut}>
+          <Icon name="exit-to-app" size={24} color={colors.red} />
+        </SignOutButton>
+      </ProfileContainer>
+    </Container>
   );
 }
---
+++ mobile/src/pages/Deliveries/styles.js
@@ -0,0 +1,49 @@
+import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
+
+import colors from '~/styles/colors';
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  padding: 20px;
+
+  background-color: ${colors.bg};
+`;
+
+export const ProfileContainer = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const ProfileContent = styled.View`
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Avatar = styled.Image`
+  height: 70px;
+  width: 70px;
+  border-radius: 35px;
+  margin-right: 20px;
+`;
+
+export const Welcome = styled.Text`
+  font-size: 12px;
+  color: ${colors.fontLight};
+`;
+
+export const UserName = styled.Text`
+  font-size: 22px;
+  font-weight: bold;
+`;
+
+export const SignOutButton = styled(RectButton)`
+  width: 40px;
+  height: 40px;
+  align-items: center;
+  justify-content: center;
+`;

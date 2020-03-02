8a944bdb3ba9a6f0a9a7d481e8e1e61731b87df4
RonaldoAntonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2020-03-02 17:43:38 -0300

    feat(mobile): problems form

--- mobile/src/pages/Problems/ProblemForm/index.js
+++ mobile/src/pages/Problems/ProblemForm/index.js
@@ -1,10 +1,41 @@
-import React, { useRef } from 'react';
+import React, { useRef, useCallback, useState } from 'react';
+import PropTypes from 'prop-types';
+import { Alert } from 'react-native';
+
+import api from '~/services/api';
+
 import Container from '~/components/PageContainer';
 
 import { FormContent, Form, Input, SubmitButton } from './styles';
 
-export default function ProblemsForm({ navigation }) {
+export default function ProblemsForm({
+  route: {
+    params: { deliveryId },
+  },
+  navigation,
+}) {
   const formRef = useRef(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = useCallback(
+    async ({ description }) => {
+      if (description.length > 400) {
+        Alert.alert('Máximo de 400 caracteres.');
+        return;
+      }
+      try {
+        setLoading(true);
+        await api.post(`/deliveries/${deliveryId}/problems`, { description });
+        setLoading(false);
+        Alert.alert('Problema cadastrado com successo');
+        formRef.current.clearField('description');
+      } catch (err) {
+        setLoading(false);
+        Alert.alert('Erro ao cadastrar o problema');
+      }
+    },
+    [deliveryId]
+  );
 
   return (
     <Container
@@ -13,9 +44,9 @@ export default function ProblemsForm({ navigation }) {
       handleBack={() => navigation.goBack()}
     >
       <FormContent>
-        <Form ref={formRef}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <Input
-            name="problem"
+            name="description"
             placeholder="Inclua aqui o problema que ocorreu na entrega"
             type="string"
             autCapitalize="none"
@@ -23,9 +54,24 @@ export default function ProblemsForm({ navigation }) {
             multiline
             textAlignVertical="top"
           />
-          <SubmitButton>Enviar</SubmitButton>
+          <SubmitButton
+            loading={loading}
+            onPress={() => formRef.current.submitForm()}
+          >
+            Enviar
+          </SubmitButton>
         </Form>
       </FormContent>
     </Container>
   );
 }
+
+ProblemsForm.propTypes = {
+  route: PropTypes.shape({
+    params: PropTypes.shape({ deliveryId: PropTypes.number.isRequired })
+      .isRequired,
+  }).isRequired,
+  navigation: PropTypes.shape({
+    goBack: PropTypes.func.isRequired,
+  }).isRequired,
+};

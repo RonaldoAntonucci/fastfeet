27936db690f8c12195cc3bbab8057f0c8cc8bfe1
RonaldoAntonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2020-03-15 17:41:21 -0300

    fix(backend): remove fake data route

--- backend/src/App/Controllers/FakeDataController.js
+++ backend/src/App/Controllers/FakeDataController.js
@@ -1,30 +1,30 @@
-import Factory from '../../../__tests__/utils/factories';
+// import Factory from '../../../__tests__/utils/factories';
 
-import Cache from '../../Lib/Cache';
+// import Cache from '../../Lib/Cache';
 
-export default {
-  async index({ query }, res) {
-    let result = [];
+// export default {
+//   async index({ query }, res) {
+//     let result = [];
 
-    if (query.deliveries) {
-      result += await Factory.createMany('Delivery', 100, {
-        start_date: new Date(),
-        end_date: new Date(),
-      });
-    }
+//     if (query.deliveries) {
+//       result += await Factory.createMany('Delivery', 100, {
+//         start_date: new Date(),
+//         end_date: new Date(),
+//       });
+//     }
 
-    if (query.problems) {
-      result += await Factory.createMany('Problem', 100);
-    }
+//     if (query.problems) {
+//       result += await Factory.createMany('Problem', 100);
+//     }
 
-    if (query.deliverymanId) {
-      result += await Factory.createMany('Delivery', 100, {
-        start_date: new Date(),
-        deliveryman_id: query.deliverymanId,
-      });
-    }
+//     if (query.deliverymanId) {
+//       result += await Factory.createMany('Delivery', 100, {
+//         start_date: new Date(),
+//         deliveryman_id: query.deliverymanId,
+//       });
+//     }
 
-    await Cache.invalidatePrefixes(['*']);
-    return res.json(result);
-  },
-};
+//     await Cache.invalidatePrefixes(['*']);
+//     return res.json(result);
+//   },
+// };
